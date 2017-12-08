e0d69d9dbab6e41dc88bc288cf07bdaf5538f076
sep <sepbot@gmail.com>
2017-12-09 10:36:18 +1100

    TOR curl alias

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -4,6 +4,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+alias tcurl='curl --socks5-hostname 127.0.0.1:9050'
+
 function preexec() {
   timer=$(($(date +%s%N)/1000000))
 }
