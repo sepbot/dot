0fa978ba0ae13d378e4054229b024b0d8f453af8
sep <sepbot@gmail.com>
2018-01-20 21:23:37 +1100

    golang and gcloud

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,8 +1,18 @@
-export PATH="$HOME/bin:$HOME/apps/gcloud/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
+export GOPATH="$HOME/workspace/go"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+if [ -f "$HOME/apps/google-cloud-sdk/path.zsh.inc" ]; then
+  source "$HOME/apps/google-cloud-sdk/path.zsh.inc"
+fi
+
+if [ -f "$HOME/apps/google-cloud-sdk/completion.zsh.inc" ]; then
+  source "$HOME/apps/google-cloud-sdk/completion.zsh.inc"
+fi
 
 alias tcurl='curl --socks5-hostname 127.0.0.1:9050'
 
