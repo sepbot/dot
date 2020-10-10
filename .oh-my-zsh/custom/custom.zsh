eee05f9b790c37a0e7035a9febf9ca4f64a737cc
sep <sepbot@gmail.com>
2020-10-10 11:44:54 +1100

    non-case sensetive gopath....

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -23,7 +23,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-export GOPATH="$HOME/Workspace/go"
+export GOPATH="$HOME/gopath"
 export PATH="$GOPATH/bin:$PATH"
 if [[ "${OS}" == 'Darwin' ]]; then
   export PATH="$HOME/.brew/opt/go/libexec/bin:$PATH"
