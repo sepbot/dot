446a5ec7fa7bb57315bda989ab493875f93c0ce8
sep <sepbot@gmail.com>
2021-08-11 16:49:51 +1000

    m1

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -2,9 +2,7 @@ umask 0077
 
 export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 
-if [[ -d "$HOME/.nsc/bin" ]]; then
-  export PATH="$HOME/.nsc/bin:$PATH"
-fi
+[[ $(uname) == 'Darwin' ]] && export PATH="$HOME/.brew/bin:$PATH"
 
 if [[ -d "$HOME/.cargo" ]]; then
   export PATH="$HOME/.cargo/bin:$PATH"
@@ -19,9 +17,6 @@ export GOPATH="$HOME/gopath"
 export PATH="$GOPATH/bin:$PATH"
 
 export PIPENV_VENV_IN_PROJECT=1
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
 export EDITOR=vim
 export GRAPHVIZ_DOT="$(which dot)"
 
@@ -32,49 +27,4 @@ unalias gb
 
 eval "$(pyenv init -)"
 eval "$(direnv hook zsh)"
-
-autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C "$HOME/bin/nomad" nomad
-
-alias ide='nohup idea . > /dev/null 2>&1 &; disown'
-
-case "$(uname -a)" in
-  *microsoft*)
-    alias cls='tput clear'
-    export LOCALHOST="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-    export HOST_MACHINE="$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')"
-    export PULSE_SERVER="tcp:${HOST_MACHINE}"
-
-    export DISPLAY=":0"
-   if ! pgrep wsld >> /dev/null 2>&1 ; then
-     nohup wsld > /dev/null < /dev/null 2>&1 &
-     disown
-       while ! xset q > /dev/null 2>&1 ; do
-         sleep 0.3
-       done
-   fi
-
-     [[ -d "/opt/mssql-tools/bin" ]] && export PATH="/opt/mssql-tools/bin:${PATH}"
-     alias aseprite='/mnt/c/Program\ Files/Aseprite/Aseprite.exe'
-
-     /usr/bin/keychain --quiet --nogui $HOME/.ssh/id_rsa
-     source $HOME/.keychain/$(hostname)-sh
-     ssh-add $HOME/.ssh/id_rsa 2>/dev/null
-     ;;
-esac
-
-
-if command -v kubectl &> /dev/null; then
-  source <(kubectl completion zsh)
-fi
-
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
-
-
-if [ -d "$HOME/google-cloud-sdk" ]; then
-  export CLOUDSDK_PYTHON="/usr/bin/python"
-  . "$HOME/google-cloud-sdk/path.zsh.inc"
-  . "$HOME/google-cloud-sdk/completion.zsh.inc"
-fi
 
